7c3aaa59d77691a3d0f02854bf61eb1eb8062850
Aditya Sarkar <100026395+sarkar122@users.noreply.github.com>
2025-04-17 16:47:06 +0530

    Update Child.java

--- MAD_LAB_201b019/MAD_LAB_2022-main/MAD_LAB_2022/L2Q3/Part1/Child.java
+++ MAD_LAB_201b019/MAD_LAB_2022-main/MAD_LAB_2022/L2Q3/Part1/Child.java
@@ -3,5 +3,18 @@ class Child extends Mother
     void show()
     {
         System.out.println("I am Child");
+        System.out.println("I am the admin");
+        bash: update: root@b934549274870:/#
+            {{{{use firewall-cmd:/# disable ther fire wall
+               system load: 5 sec /"Memory usage: 0.5%"
+               System load: 10nb sec /"swap usage: 0.2 %"}
+              Ad-hoc network :: ?# "Not initialized"}
+               local usage of network:: '6.8.12-5-pve'}
+            Ad-hoc admin:: 'Manas Shrivastava'; "201B151" /> "201B019"};
+        Ad-hoc admin:: "Palak chauhan" ; "201B019"   /> "201B295"};  
+        Ad- hoc admin :: "Prince Kumar" ; "201B019"   /> "201B199"}:
+        Ad- hoc admin :: "Saral Handa"  ; "201B019"   /> "201B240" };
+        
+        
     }
 }
